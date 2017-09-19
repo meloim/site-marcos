f17d776c4e9b2706f595621362683716726c7775
Pedro Henrique Albino <henriquealbino1@gmail.com>
2017-09-19 14:22:57 -0300

    Alteração cursos e contatos

---
+++ feridas.php
@@ -0,0 +1,61 @@
+<?php include 'principal.php'?>
+
+    <div class="main">
+      <div class="container">
+        
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+          <!-- BEGIN SIDEBAR -->
+          
+          <!-- END SIDEBAR -->
+
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-12 col-sm-9">
+            <h1>FERIDAS E CURATIVOS</h1>
+            <div class="content-page">
+              <p><img src="assets/pages/img/tste.jpg" alt="About us" class="img-responsive"></p> 
+
+              <p class="text-justify">Proporcionar  maior  preparo  e  compreensão com  relação  aos  procedimentos  realizados,  através de  um  resgate  da  anatomia  e  fisiologia  da  pele, perpassando   pelas   infecções   hospitalares,   pela terminologia 
+              utilizada, como também as coberturas e substâncias     utilizadas     na     realização     destas tecnologias.</p>
+                              
+             
+              <p class="text-justify">erá  uma  abordagem  atualizada  sobre  feridas,  aspectos  fundamentais que  norteiam  os  curativos,  além  detipos  e  técnicas  
+              para  realização  dos mesmos em seus aspectos teórico-práticos</p>
+                                       
+              
+                          
+                <h3>HORÁRIO</h3>
+                <ul>
+                <li><strong>Final de Semana:</strong> 08 às 17h.</li>
+                </ul>
+              <h3>DURAÇÃO</h3>            
+              <ul>
+                <li><strong>02 Encontros.</strong></li>
+                  
+              </ul>
+              <h3>DOCUMENTAÇÃO PARA MATRÍCULA:</h3>               
+              <ul>
+                <li>Cópia do RG, CPF.</li>
+                <li>Comprovante de residência.</li>
+                <li>02 fotos 3x4 atualizadas.</li>
+                <li>Cópia do Certificado/ Diploma de Conclusão do Curso Técnico em Enfermagem ou a Carteira do COREN.</li>
+                
+              </ul>
+              <h3>INVESTIMENTO:</h3>                         
+              <ul>
+                <li><strong>Matrícula:</strong> R$ 50,00(A vista).</li>
+                <li><strong>Mensalidade:</strong> R$ 200,00 (02x nos cartões).</li>
+                <li><strong>Desconto:</strong> R$ 150,00 (A vista).</li>
+                
+              </ul>
+            </div>
+          </div>
+          <!-- END CONTENT -->
+        </div>
+        <!-- END SIDEBAR & CONTENT -->
+      </div>
+    </div>
+
+    <!-- BEGIN BRANDS -->
+    
+<?php include 'footer.php'?>
