74a13b17fe8932a8e906bc4edad39e2121facaab
meloim <matheus.eloim@gmaill.com>
2017-09-22 16:03:03 -0300

    sem php

---
+++ controller.js
@@ -0,0 +1,4 @@
+var app = angular.module('siteMarcos', []);
+app.controller('customersCtrl', function($scope, $http) {
+
+});
---
+++ temp/php/suporte-vida.php
@@ -0,0 +1,62 @@
+<?php include 'principal.php'?>
+
+    <div class="main">
+      <div class="container">
+        
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+          <!-- BEGIN SIDEBAR -->
+          
+          <!-- END SIDEBAR -->
+
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-12 col-sm-9">
+            <h1>SUPORTE BÁSICO DE VIDA (BLS/HAH/2015/PCR/RCP)</h1>
+            <div class="content-page">
+              <p><img src="assets/pages/img/ambulancia.jpg" alt="About us" class="img-responsive"></p> 
+
+              <p class="text-justify"><strong>Suporte  básico  de  vida</strong> (SBV)  é  o  conjunto  de medidas   e   procedimentos   técnicos   que   objetivam   o suporte de vida à vítima. O SBV é vital até a chegada do SIV   (Suporte   intermediário   de   vida -transporte   até ohospital).  O  objetivo  principal  é  não  agravar  lesões 
+               já existentes ou gerar novas lesões (iatrogenias). Um rápido SBV proporciona até 60% de chance de sobrevivência.</p>
+                              
+             
+              <p class="text-justify">O Suporte  básico de vida é um dos elos da "Cadeia de Sobrevivência". 
+              Há que salientar também que no SBV (Suporte Básico da Vida) tem diferença entre bebês,  crianças  e  adultos,
+                poisnos bebêsusam-se  dois  dedos  para  se fizera compressão, nas crianças usa-se uma mão para se fazer a compressão. 
+              As   semelhanças   são   os   processos   de   insuflação   que   tem   o   mesmo procedimento
+                </p>
+              
+                          
+                <h3>HORÁRIO</h3>
+                <ul>
+                <li><strong>Final de Semana:</strong> 08 às 17h.</li>
+                </ul>
+              <h3>DURAÇÃO</h3>            
+              <ul>
+                <li><strong>02 Encontros.</strong></li>
+                  
+              </ul>
+              <h3>DOCUMENTAÇÃO PARA MATRÍCULA:</h3>               
+              <ul>
+                <li>Cópia do RG, CPF.</li>
+                <li>Comprovante de residência.</li>
+                <li>02 fotos 3x4 atualizadas.</li>
+                
+              </ul>
+              <h3>INVESTIMENTO:</h3>                         
+              <ul>
+                <li><strong>Matrícula:</strong> R$ 50,00(A vista).</li>
+                <li><strong>Mensalidade:</strong> R$ 200,00 (02x nos cartões).</li>
+                <li><strong>Desconto:</strong> R$ 150,00 (A vista).</li>
+                
+              </ul>
+            </div>
+          </div>
+          <!-- END CONTENT -->
+        </div>
+        <!-- END SIDEBAR & CONTENT -->
+      </div>
+    </div>
+
+    <!-- BEGIN BRANDS -->
+    
+<?php include 'footer.php'?>
